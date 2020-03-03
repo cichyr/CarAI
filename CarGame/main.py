eaa6a925563d67624f1ea93f4cca4340afffc0db
Dominik Wolny <wolny.dominik@outlook.com>
2020-03-03 23:17:25 +0100

    Implemented car handling
    Car now goes forward and backward, turns properly, may need further refactor, but for now it's okay.

--- CarGame/main.py
+++ CarGame/main.py
@@ -2,16 +2,24 @@ import arcade
 import os
 import sys
 import time
+import math
 
 
 class Car():
 
-    def __init__(self, x, y, a, d):
+    def __init__(self, x, y, acceleration, deceleration, rotation):
         self.x = x
         self.y = y
-        self.acceleration = a
-        self.deceleration = d
+        self.acceleration = acceleration
+        self.deceleration = deceleration
+        self.rotation = rotation
         self.velocity = 0
+
+    def get_velocity_y(self):
+        return self.velocity * math.sin(math.radians(self.rotation))
+
+    def get_velocity_x(self):
+        return self.velocity * math.cos(math.radians(self.rotation))
 
 
 class Helper():
@@ -31,10 +39,8 @@ class CarGame(arcade.Window):
 
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.WHITE)
-        self.car = Car(300, 50, 0.1, 0.2)
+        self.car = Car(300, 50, 0.1, 0.2, 90)
         self.helper = Helper()
-        #self.old = True
-        self.old = False
 
     # Prepare Tile Array
     def setup(self):
@@ -47,7 +53,7 @@ class CarGame(arcade.Window):
         arcade.start_render()
 
         arcade.draw_rectangle_filled(
-            self.car.x, self.car.y, 45, 65, arcade.color.RED)
+            self.car.x, self.car.y, 65, 45, arcade.color.RED, self.car.rotation)
 
     # Handle user key press
     def on_key_press(self, key, modifiers):
@@ -55,21 +61,21 @@ class CarGame(arcade.Window):
         # Car movement    
         if key == arcade.key.W:
             self.helper.W = True
-        elif key == arcade.key.S:
+        if key == arcade.key.S:
             self.helper.S = True
-        elif key == arcade.key.A:
+        if key == arcade.key.A:
             self.helper.A = True
-        elif key == arcade.key.D:
+        if key == arcade.key.D:
             self.helper.D = True
 
         # Strafe left/right for now
-        elif key == arcade.key.A:
-            self.car.x -= 10
-        elif key == arcade.key.D:
-            self.car.x += 10
+        #elif key == arcade.key.A:
+        #    self.car.x -= 10
+        #elif key == arcade.key.D:
+        #    self.car.x += 10
 
         # Exit game
-        elif key == arcade.key.Q:
+        if key == arcade.key.Q:
             arcade.window_commands.close_window()
 
     # Handle user key release
@@ -78,11 +84,11 @@ class CarGame(arcade.Window):
         # Car movement
         if key == arcade.key.W:
             self.helper.W = False
-        elif key == arcade.key.S:
+        if key == arcade.key.S:
             self.helper.S = False
-        elif key == arcade.key.A:
+        if key == arcade.key.A:
             self.helper.A = False
-        elif key == arcade.key.D:
+        if key == arcade.key.D:
             self.helper.D = False
 
     # Handle screen updates
@@ -91,15 +97,31 @@ class CarGame(arcade.Window):
         # Handle car velocity change
         if self.helper.W and self.car.velocity >= 0:
             self.car.velocity += self.car.acceleration
-        elif self.helper.W and self.car.velocity < 0:
+        if self.helper.W and self.car.velocity < 0:
             self.car.velocity += self.car.deceleration
-        elif self.helper.S and self.car.velocity > 0:
+        if self.helper.S and self.car.velocity > 0:
             self.car.velocity -= self.car.deceleration
-        elif self.helper.S and self.car.velocity <= 0:
+        if self.helper.S and self.car.velocity <= 0:
             self.car.velocity -= self.car.acceleration
+        # Handle car rotation
+        if self.helper.A:
+            self.car.rotation += 5
+        if self.helper.D:
+            self.car.rotation -= 5
 
-        # Handle car position change (TODO xD)
-        self.car.y += self.car.velocity
+        # Handle car position change
+        self.car.y += self.car.get_velocity_y()
+        self.car.x += self.car.get_velocity_x()
+
+        # Loop car on border, just for lols and not losing the car
+        if self.car.x > self.get_size()[0]:
+            self.car.x = 0
+        elif self.car.x < 0:
+            self.car.x = 600
+        if self.car.y > self.get_size()[1]:
+            self.car.y = 0
+        elif self.car.y < 0:
+            self.car.y = 600
 
         self.on_draw()
 
